721fa007fa7b8ac2a6e063b4ac0c3dbe679cf729
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2023-05-25 20:18:26 -0400

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
 import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Nav from "./Components/Navigation/Nav";
 import Recipes from "./Pages/Recipes";
+import RecipeInfo from "./Pages/RecipeInfo";
 
 const App = () => {
-
-  const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
   const [recipeData, setRecipeData] = useState([]);
+ 
 
-
-  // todo: create custom hook for API calls 
+  // todo: create custom hook for API calls
   async function fecthData() {
     const response = await fetch(
       `https://api.spoonacular.com/recipes/complexSearch?apiKey=9a988b71457d457fbe5c178fc9c8bcd4&query=${searchInput}&addRecipeInformation=true&fillIngredients=true&instructionsRequired=true`
     );
     const data = await response.json();
-    setRecipeData(data.results)
+    setRecipeData(data.results);
     console.log(data.results);
   }
 
-  // todo: implement react router 
+  
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Nav setSearchInput={setSearchInput} fecthData={fecthData} />,
+      children: [
+        { path: "/", element: <Recipes recipeData={recipeData} /> },
+        { path: "/recipe/:recipeId", element: <RecipeInfo /> },
+      ],
+    },
+  ]);
+
+  // todo: implement react router
   return (
     <>
-      <Nav setSearchInput={setSearchInput} fecthData={fecthData} />
-      <Recipes recipeData={recipeData}/>
+      <RouterProvider router={router} />
     </>
   );
 };
--- src/Pages/Recipes.jsx
+++ src/Pages/Recipes.jsx
@@ -1,13 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Recipes = (props) => {
   return (
     <>
       {props.recipeData.map((recipe) => {
         return (
           <>
-            <div className="mt-3 grid grid-cols-1 justify-items-center gap-1">
+            {/* todo: add styling for tablet and desktop  */}
+            <div key={recipe.id} className="mt-3 grid grid-cols-1 justify-items-center gap-1">
               <h1>{recipe.title}</h1>
-              <img src={recipe.image} alt={recipe.title} className="rounded-xl" />
-              <p className="text-blue-200">Show Full Recipe</p>
+              <img
+                src={recipe.image}
+                alt={recipe.title}
+                className="rounded-xl"
+              />
+              <Link to={`/recipe/${recipe.id}`}>Show Full Recipe</Link>
             </div>
           </>
         );
