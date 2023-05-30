253bb06cc972e28c1932fa8774280a790b3518c4
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2023-05-30 18:09:55 -0400

    key warning fix

--- src/Pages/Recipes.jsx
+++ src/Pages/Recipes.jsx
@@ -5,28 +5,26 @@ const Recipes = (props) => {
     <>
       {props.recipeData.map((recipe) => {
         return (
-          <>
-            {/* todo: add styling for tablet and desktop  */}
-            <div
+          <div
+            key={recipe.id}
+            className="mt-3 grid grid-cols-1 justify-items-center gap-1"
+          >
+            <h1>{recipe.title}</h1>
+            <img
+              src={recipe.image}
+              alt={recipe.title}
               key={recipe.id}
-              className="mt-3 grid grid-cols-1 justify-items-center gap-1"
-            >
-              <h1>{recipe.title}</h1>
-              <img
-                src={recipe.image}
-                alt={recipe.title}
-                className="rounded-xl"
-              />
-              <p>
-                <Link
-                  to={`/recipeInfo/${recipe.id}`}
-                  onClick={props.getIngredients.bind(null, recipe.id)}
-                >
-                  Show Full Recipe
-                </Link>
-              </p>
-            </div>
-          </>
+              className="rounded-xl"
+            />
+            <p>
+              <Link
+                to={`/recipeInfo/${recipe.id}`}
+                onClick={props.getIngredients.bind(null, recipe.id)}
+              >
+                Show Full Recipe
+              </Link>
+            </p>
+          </div>
         );
       })}
     </>
