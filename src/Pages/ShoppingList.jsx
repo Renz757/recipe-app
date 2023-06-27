3986357540e9a4407d5b22fe680764d376b24a44
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2023-06-27 19:54:59 -0400

    added checkbox, removeIcon, and checkIcon

--- src/Pages/ShoppingList.jsx
+++ src/Pages/ShoppingList.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import CheckIcon from "../UI/checkIcon";
+import RemoveIcon from "../UI/removeIcon";
 
 const ShoppingList = ({ shoppingList }) => {
   return (
@@ -9,10 +11,22 @@ const ShoppingList = ({ shoppingList }) => {
         {shoppingList.map((recipeIngredients, index) => {
           return (
             <div key={index} className="flex flex-col gap-3 justify-center">
-              <h2 className="pt-5 text-3xl font-Caveat">{recipeIngredients.title}</h2>
+              <div className="pt-5 flex items-center gap-3">
+                <h1 className="text-3xl font-Caveat">
+                  {recipeIngredients.title}
+                </h1>
+                <CheckIcon />
+                <RemoveIcon />
+              </div>
               <ul>
                 {recipeIngredients.ingredients.map((items) => (
-                  <li className="font-noto text-lg p-1">{items}</li>
+                  <li className="font-noto text-lg p-1 flex gap-2 items-center">
+                    <input
+                      className="h-5 w-5 accent-green-400"
+                      type="checkbox"
+                    />
+                    <div className="w-10/12">{items}</div>
+                  </li>
                 ))}
               </ul>
             </div>
