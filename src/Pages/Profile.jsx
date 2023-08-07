f1034c90bf2d093f859a1b73d55ee4c087a3f37d
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2023-08-07 19:45:43 -0400

    spacing fixes

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -9,13 +9,13 @@ const Profile = () => {
 
   const user = useSelector((state) => state.auth.user);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const logoutHandler = async () => {
     try {
       setError("");
       await auth.signOut();
-      navigate("/login")
+      navigate("/login");
     } catch (error) {
       setError("There was an issue  singing out");
     }
@@ -25,14 +25,11 @@ const Profile = () => {
   return (
     <>
       {!user && <p>Please Log in</p>}
-      <div className="w-screen h-screen bg-eggshell text-center text-xl flex flex-col p-32">
-        <div className="flex items-center gap-x-3">
-          <h2 className="">User Name:</h2>
-          <p className="">{user.email}</p>
-        </div>
-        <div onClick={logoutHandler} className="flex justify-center gap-x-3">
-          <h3 className="font-noto">Logout</h3>
-          <LogoutIcon />
+      <div className="w-screen h-screen font-noto bg-eggshell flex flex-col text-center p-32 text-2xl">
+        <h2 className="mb-5">User Name: {user.email}</h2>
+
+        <div onClick={logoutHandler} className="flex justify-center">
+          <h2>Logout</h2> <LogoutIcon />
         </div>
       </div>
     </>
