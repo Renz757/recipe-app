e2b84794b3c2cdca4a27aaaf9a5cd097f19e5e18
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2023-08-26 11:19:03 -0400

    font and color styling, add btn changes

--- src/Pages/RecipeInfo.jsx
+++ src/Pages/RecipeInfo.jsx
@@ -139,7 +139,7 @@ const RecipeInfo = ({ recipeInfoId }) => {
                 />
               </div>
               <div className="flex items-center justify-between p-3">
-                <h1 className="text-4xl font-Caveat">
+                <h1 className="text-4xl font-Caveat text-vandyke">
                   {isNotCustomRecipe
                     ? recipeInfo.title
                     : customRecipeInfo.title}
@@ -172,17 +172,17 @@ const RecipeInfo = ({ recipeInfoId }) => {
                 </div>
               </div>
               <div className="font-noto w-10/12 mx-auto">
-                <p className="mt-2 p-2 border-b-2 border-zinc-500">{`Ready In: ${
+                <p className="mt-2 p-2 border-b-2 border-vandyke">{`Ready In: ${
                   isNotCustomRecipe
                     ? recipeInfo.readyInMinutes
                     : customRecipeInfo.cookTime
                 } minutes`}</p>
-                <p className="mt-2 p-2 border-b-2 border-zinc-500">{`Servings: ${
+                <p className="mt-2 p-2 border-b-2 border-vandyke">{`Servings: ${
                   isNotCustomRecipe
                     ? recipeInfo.servings
                     : customRecipeInfo.servingSize
                 }`}</p>
-                <p className="mt-2 p-2 border-b-2 border-zinc-500">{`Ingredients: ${
+                <p className="mt-2 p-2 border-b-2 border-vandyke">{`Ingredients: ${
                   isNotCustomRecipe
                     ? recipeInfo.extendedIngredients.length
                     : customRecipeInfo.ingredients.length
@@ -191,8 +191,8 @@ const RecipeInfo = ({ recipeInfoId }) => {
             </div>
 
             {/* Ingredients */}
-            <div className="flex flex-col mt-16 px-5 py-7 border-t-2 border-zinc-500">
-              <h2 className="text-2xl text-zinc-600 font-Geologica">
+            <div className="flex flex-col mt-16 px-5 py-7 border-t-2 border-vandyke">
+              <h2 className="text-2xl text-vandyke font-Geologica">
                 Ingredients
               </h2>
               {isNotCustomRecipe
@@ -226,14 +226,14 @@ const RecipeInfo = ({ recipeInfoId }) => {
                         customRecipeInfo.title
                       )
                 }
-                className="text-center mt-5 bg-green-400 w-full md:w-6/12 p-2 rounded-xl cursor-pointer"
+                className="text-center text-lg font-noto mt-5 border-vandyke border-2 w-full md:w-6/12 p-2 rounded cursor-pointer hover:bg-vandyke hover:text-eggshell transition-all ease-in-out"
               >
                 Add Ingredients to Shopping List
               </button>
             </div>
 
-            <div className="flex flex-col mt-16 px-5 py-7 border-t-2 border-zinc-500 pb-10">
-              <h1 className="text-2xl text-zinc-600 font-Geologica">
+            <div className="flex flex-col mt-16 px-5 py-7 border-t-2 border-vandyke pb-10">
+              <h1 className="text-2xl text-vandyke font-Geologica">
                 Instructions
               </h1>
               {isNotCustomRecipe
